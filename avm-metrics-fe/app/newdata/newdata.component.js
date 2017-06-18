81334bd22050611a09f957220327a371959c4c19
Kerea <jadelangel87@gmail.com>
2017-06-18 12:38:43 -0500

    Setup template view for assets added

--- avm-metrics-fe/app/newdata/newdata.component.js
+++ avm-metrics-fe/app/newdata/newdata.component.js
@@ -9,6 +9,39 @@ exports.__esModule = true;
 var core_1 = require("@angular/core");
 var NewdataComponent = (function () {
     function NewdataComponent() {
+        this.pageTitle = "Asset Dashboard";
+        this.newdata = [
+            {
+                asset_id: 52252,
+                host_name: "TEST-LAPTOP67801",
+                IP: "192.168.1.149",
+                OS: "Microsoft Windows 7 Professional Edition SP1",
+                critical_vul: 173,
+                severe_vul: 451,
+                risk_score: 285080,
+                last_discovered: '06/10/2017'
+            },
+            {
+                asset_id: 9487,
+                host_name: "TEST-PC63285",
+                IP: "192.168.1.115",
+                OS: "Microsoft Windows 7 Professional Edition SP1",
+                critical_vul: 875,
+                severe_vul: 616,
+                risk_score: 86449,
+                last_discovered: '06/06/2017'
+            },
+            {
+                asset_id: 1925,
+                host_name: "TEST-TABLET63327",
+                IP: "192.168.1.64",
+                OS: "Microsoft Windows 7 Professional Edition SP1",
+                critical_vul: 70,
+                severe_vul: 311,
+                risk_score: 129091,
+                last_discovered: '06/09/2017'
+            }
+        ];
     }
     return NewdataComponent;
 }());
