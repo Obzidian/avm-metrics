25d74070b4fd8d6b544c6eabaccd7e8639417e21
Kerea <jadelangel87@gmail.com>
2017-06-18 00:46:45 -0500

    Built out base components for data import feature

--- avm-metrics-fe/app/app-routing.module.ts
+++ avm-metrics-fe/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomepageComponent } from './homepage/homepage.component';
+import { NewdataComponent } from './newdata/newdata.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomepageComponent },
+  { path: 'newdata', component: NewdataComponent },
 ]
 
 @NgModule({
--- avm-metrics-fe/app/homepage/homepage.component.ts
+++ avm-metrics-fe/app/homepage/homepage.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'app',
-  templateUrl: 'homepage.component.html';
+  templateUrl: 'homepage.component.html'
 })
 export class HomepageComponent {}
